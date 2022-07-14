7eae3ebaf3b2944cb15e80e119715d92573f7e17
Logvinov Roman <roman.logvinov88@gmail.com>
2022-07-14 22:48:43 +0300

    fixed, add option onClose and callbackack

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+console.log(galleryItems);
+
+
+
 const onCreateGallery = (items) => {
     return items.map(({ preview, original, description }) =>
         `<div class="gallery__item">
@@ -15,6 +19,7 @@ const onCreateGallery = (items) => {
         .join('');
 };
 
+
 const clickOnGalleryBox = (e) => {
     e.preventDefault();
     if (e.target.nodeName !== 'IMG') { return };
@@ -23,18 +28,23 @@ const clickOnGalleryBox = (e) => {
         `<img src="${e.target.dataset.source}">`,
         {
             onShow:  () => {
-                window.addEventListener("keydown", (e) => {if (e.code === "Escape") {
-            modalWindow.close();
-        }})
+                window.addEventListener("keydown", handlePressKey)
             },
+            onClose: () => {
+                window.removeEventListener("keydown", handlePressKey)
+            }
         }
     )
     modalWindow.show();
+    
+    function handlePressKey(evt) {
+        if (evt.code === "Escape") {
+            modalWindow.close();
+        }
+    }
 };
 
 const galleryBox = document.querySelector('.gallery');
 const galleryMarkup = onCreateGallery(galleryItems);
 galleryBox.insertAdjacentHTML("afterbegin", galleryMarkup);
 galleryBox.addEventListener('click', clickOnGalleryBox);
-
-
